4a2c77366ec492239161d39c1bde2e8cdf598d17
Yicheng Shen <jsszsyc@hotmail.com>
2021-06-18 15:20:25 +0000

    add gitpod related scripts

---
+++ leaf_version/prep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# file id of data stored on google drive
+FILE_ID="1TVpUvGNGLgdZBDDSGQoOIzHz4U8TgHcT"
+
+# Install gdown (for downloading data)
+pip install gdown
+
+# Download data
+gdown --id $FILE_ID
+mv -n femnist.zip ../data
+unzip -f ../data/femnist.zip
+rm ../data/femnist.zip
+
+# Install Python dependencies
+pip install -r ../requirements.txt
